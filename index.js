bf2a426c9003479026f2b08a77a551bb60a14193
Ariun <thiagotatico007@gmail.com>
2022-04-07 10:15:16 -0300

    Changes

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ require('dotenv').config();
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Call and execute the Event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Call and execute the Command files
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
+	// With the key as the command name and the value as the exported module :)
 	client.commands.set(command.data.name, command);
 }
-
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
 
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
