4d09f44ba69a02ea834eeaf4ab5d1fa2ac497452
Ariun <thiagotatico007@gmail.com>
2022-04-07 15:04:25 -0300

    Add Commands

---
+++ commands/qr-code.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('qr-code')
+		.setDescription(' generates QR codes')
+		.addStringOption(option => option.setName('input').setDescription('The input to echo back')),
+	async execute(interaction) {
+		const value = interaction.options.getString('input');
+		const url = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${value}`;
+		if (url) return interaction.reply(`${url}`);
+		return interaction.reply('No option was provided!');
+	},
+};
